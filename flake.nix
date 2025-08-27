05a0978ef255b8d12e295804f8c05f7decc217f0
uPiscium <upiscium@gmail.com>
2025-08-27 11:26:48 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+      {
+        devShells.x86_64-linux.default = {
+          packages = with pkgs; [
+            uv
+          ];
+        };
+      };
+  };
+}
