b9ce8be8d8bc6f6cad6d482a26ff9ce404971124
uPiscium <upiscium@gmail.com>
2025-09-14 22:38:30 +0900

    Update README and add port option

--- src/entry.py
+++ src/entry.py
@@ -14,7 +14,14 @@ if __name__ == "__main__":
         required=True,
         help="endpoint of control server",
     )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="port to run the server on (default: 8000)",
+    )
     args = parser.parse_args()
 
     app = App(args.database_path, args.control_server_endpoint)
-    uvicorn.run(app.get_app(), host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app.get_app(), host="0.0.0.0", port=args.port, log_level="info")
